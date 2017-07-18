d1c07b5f0f2a177cd5e791c1f85572fa78469bb5
Cameron Jenner <cjenner37@gmail.com>
2017-07-18 15:10:13 -0600

    basic validations done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :images
-  has_many :comments
+  has_many :comments, dependent: :destroy
+
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
    has_many :workouts
-   has_many :subscriptions
+   has_many :subscriptions, dependent: :destroy
    has_many :workouts, through: :subscriptions
-   has_many :posts
-   has_many :comments
+   has_many :posts, dependent: :destroy
+   has_many :comments, dependent: :destroy
+
+   validates :first_name, presence: true
 
    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
