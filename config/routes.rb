63ca8d9deb2b420168b868436c82eb3c26436370
Cameron Jenner <cjenner37@gmail.com>
2017-07-18 16:01:10 -0600

    padding on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
      sessions: 'users/sessions',
      registrations: 'users/registrations'
-   }
+  }
 	root to: "home#index"
 
 end
